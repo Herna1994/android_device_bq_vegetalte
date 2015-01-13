6992dd27aa243e44a4d7586819abcf08702d7ee6
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2015-01-13 15:39:31 -0800

    Rename bq_aquarise5 to the original device codename "vegetalte"

--- omni_aquarise5.mk
+++
@@ -1,9 +0,0 @@
-# Inherit device configuration
-$(call inherit-product, device/bq/aquarise5/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := aquarise5
-PRODUCT_NAME := omni_aquarise5
-PRODUCT_BRAND := bq
-PRODUCT_MANUFACTURER := bq
-PRODUCT_MODEL := Aquaris E5
